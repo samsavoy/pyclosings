97d6af0e21dab34d89151740e231c376d193ba8d
samsavoy <forms@samsavoy.com>
2015-02-05 20:09:04 -0500

    Removed duplicate list creation in printList

--- closings.py
+++ closings.py
@@ -5,7 +5,7 @@ def main():
 	content = urllib.request.urlopen('http://local.wnep.com/transfers/school.html')
 	content = content.read()
 	soup = BeautifulSoup(content)
-	schools = soup.find_all("font", class_="orgname")
+	schools = soup.find_all("font",class_="orgname")
 	statuses = soup.find_all("font",class_="status")
 	list = []
 	counter = 0
@@ -15,11 +15,7 @@ def main():
 	printList(list)
 
 def printList(list):
-	schools = []
-	statuses = []
 	for updates in list:
-		schools.append(updates[0])
-		statuses.append(updates[1])
-		print(schools[-1],":", statuses[-1])
+		print(updates[0],":",updates[1])
 
 main()
